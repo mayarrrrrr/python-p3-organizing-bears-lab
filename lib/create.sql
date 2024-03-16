fc77f2426f553806937f8f330e465805df883a6d
mayarakok <akokmayar607@gmail.com>
2024-03-16 12:45:42 +0300

    sql queries

---
+++ lib/create.sql
@@ -0,0 +1,13 @@
+-- Active: 1710571153543@@127.0.0.1@3306
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+
+
+
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Mr. Chocolate", 3, "M","brown",NULL, true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Rowdy", 3, "F","brown",NULL, true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Tabitha", 1, "M","brown",NULL, true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Sergeant Brown", 4, "F","brown",NULL, true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Melissa", 8, "M","brown",NULL, false);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Grinch", 7, "F,.","brown",NULL, true);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES("Wendy", 4, "M","brown",NULL, false);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL, 6, "M","brown",NULL, true);
